eb0e98eec60a3214a3d58fd8a6970132b9f2ff5d
cyberhardt <107224222+cyberhardt@users.noreply.github.com>
2023-06-03 11:33:25 -0300

    Update trackers-add-all.bat

--- scripts/trackers-add-all.bat
+++ scripts/trackers-add-all.bat
@@ -1,1 +1,33 @@
-for /r %%v in ("C:\Users\-\Downloads\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a udp://open.stealth.si:80/announce -a udp://tracker.torrent.eu.org:451/announce -a udp://tracker.openbittorrent.com:6969 -a udp://tracker.opentrackr.org:1337/announce -a udp://tracker.cyberia.is:6969/announce -a http://tracker.gbitt.info:80/announce -a udp://tracker-udp.gbitt.info:80/announce -a https://tracker.gbitt.info:443/announce -a udp://tracker.leech.ie:1337/announce -a http://nyaa.tracker.wf:7777/announce -a http://sukebei.tracker.wf:8888/announce -a udp://tracker.bitsearch.to:1337/announce -a udp://open.demonii.com:1337 -a udp://open.tracker.cl:1337/announce -a udp://bt1.archive.org:6969/announce -a udp://bt2.archive.org:6969/announce "%%v"
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a udp://open.stealth.si:80/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a udp://tracker.torrent.eu.org:451/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a udp://tracker.openbittorrent.com:6969 "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a udp://tracker.opentrackr.org:1337/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a udp://tracker.cyberia.is:6969/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a http://tracker.gbitt.info:80/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a udp://tracker-udp.gbitt.info:80/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a https://tracker.gbitt.info:443/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a udp://tracker.leech.ie:1337/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a http://nyaa.tracker.wf:7777/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a http://sukebei.tracker.wf:8888/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a udp://tracker.bitsearch.to:1337/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a udp://open.demonii.com:1337 "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a udp://open.tracker.cl:1337/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a udp://bt1.archive.org:6969/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a udp://bt2.archive.org:6969/announce "%v"
+
+for %v in ("C:\torrents\*.torrent") do "C:\Program Files\Transmission\transmission-edit" -a https://tracker.bt4g.com:443/announce "%v"
